e5777ed50600e8c26da27bba392fbe705cc146ef
cylinc <xuyuanwei@gmail.com>
2013-10-09 22:49:10 +0800

    trace: add perspective transform

--- trace/warp_affine.cpp
+++ trace/warp_affine.cpp
@@ -1,3 +1,7 @@
+/*
+ * reference: http://www.opencv.org.cn/opencvdoc/2.3.2/html/doc/tutorials/imgproc/imgtrans/warp_affine/warp_affine.html 
+ * keyword: affineTransform 仿射变化; perspectiveTransform 透视变化
+ */
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include <iostream>
@@ -7,9 +11,10 @@ using namespace cv;
 using namespace std;
 
 /// 全局变量
-char* source_window = "Source image";
-char* warp_window = "Warp";
-char* warp_rotate_window = "Warp + Rotate";
+char source_window[] = "Source image";
+char warp_window[] = "Warp";
+char warp_rotate_window[] = "Warp + Rotate";
+char perspective_window[] = "perspective transform";
 
 /** @function main */
 int main( int argc, char** argv )
@@ -17,15 +22,22 @@ int main( int argc, char** argv )
     Point2f srcTri[3];
     Point2f dstTri[3];
 
+    Point2f srcTri_perspective[4];
+    Point2f dstTri_perspective[4];
+
     Mat rot_mat( 2, 3, CV_32FC1 );
     Mat warp_mat( 2, 3, CV_32FC1 );
     Mat src, warp_dst, warp_rotate_dst;
+
+    Mat perspective_mat(3,3,CV_32FC1);
+    Mat perspective_dst;
 
     /// 加载源图像
     src = imread( argv[1], 1 );
 
     /// 设置目标图像的大小和类型与源图像一致
     warp_dst = Mat::zeros( src.rows, src.cols, src.type() );
+    perspective_dst=Mat::zeros(src.rows*2, src.cols*2, src.type() );
 
     /// 设置源图像和目标图像上的三组点以计算仿射变换
     srcTri[0] = Point2f( 0,0 );
@@ -42,8 +54,30 @@ int main( int argc, char** argv )
     /// 对源图像应用上面求得的仿射变换
     warpAffine( src, warp_dst, warp_mat, warp_dst.size() );
 
+    namedWindow( source_window, CV_WINDOW_AUTOSIZE );
+    imshow( source_window, src );
+
+    namedWindow( warp_window, CV_WINDOW_AUTOSIZE );
+    imshow( warp_window, warp_dst );
+
+    srcTri_perspective[0] = Point2f( 98, 337 );
+    srcTri_perspective[1] = Point2f( 278,307 );
+    srcTri_perspective[2] = Point2f( 98, 98  );
+    srcTri_perspective[3] = Point2f( 306,69  );
+
+    dstTri_perspective[0] = Point2f( 70, 366 );
+    dstTri_perspective[1] = Point2f( 339,336 );
+    dstTri_perspective[2] = Point2f( 128,125 );
+    dstTri_perspective[3] = Point2f( 247,97  );
+
+    perspective_mat=getPerspectiveTransform(srcTri_perspective,dstTri_perspective);
+    warpPerspective(src,perspective_dst,perspective_mat,perspective_dst.size());
+    namedWindow( warp_window, CV_WINDOW_AUTOSIZE );
+    imshow( perspective_window, perspective_dst );
+
     /** 对图像扭曲后再旋转 */
 
+    /*
     /// 计算绕图像中点顺时针旋转50度缩放因子为0.6的旋转矩阵
     Point center = Point( warp_dst.cols/2, warp_dst.rows/2 );
     double angle = -50.0;
@@ -64,6 +98,7 @@ int main( int argc, char** argv )
 
     namedWindow( warp_rotate_window, CV_WINDOW_AUTOSIZE );
     imshow( warp_rotate_window, warp_rotate_dst );
+    */
 
     /// 等待用户按任意按键退出程序
     waitKey(0);
