3f40e40122d5a663748540499a96b9beaccf1ab5
cylinc <xuyuanwei@gmail.com>
2013-10-19 23:57:24 +0800

    use sin/cos way to draw a circle

--- trace/reversetrack.m
+++ trace/reversetrack.m
@@ -4,56 +4,40 @@ b=20/180*pi;    # angle between camera center line and horizon line
 h=1;            # camera hight from ground
 hscreen=0.1;    # screen hight
 wscreen=0.2;    # screen width
-l=2.87;            # car wheelbase(÷·æ‡)
-w=1.56;          # car front/rear wheel distance(«∞/∫Û¬÷æ‡)
-d=1.03;          # distance between rear wheel to back of car
+l=2.87;         # car wheelbase(÷·æ‡)
+w=1.56;         # car front/rear wheel distance(«∞/∫Û¬÷æ‡)
+d=1.03;         # distance between rear wheel to back of car
 phi=10/180*pi;  # the angle between front wheelbase and horizon line
 
-x_orig=l*cot(phi);
+
+#   draw cricle reference:
+#   http://stackoverflow.com/questions/7971467/how-to-draw-a-circle
+
+
+for i=1:9
+    R_middle_point=l*cot(phi);
+    t=linspace(0,pi,100);
+    Xoffset=R_middle_point;
+    Yoffset=d;
 # big round
-#x=linspace(-l*cot(phi),w/2,100);
-r=l*cot(phi)+w/2;
-# the x value when y=0
-x=linspace(-sqrt(power(r,2)-power(d,2))-x_orig,+sqrt(power(r,2)-power(d,2))-x_orig,100);
-y=sqrt(power((l*cot(phi)+w/2),2)-power((x+l*cot(phi)),2))-d;
+    R_big_round=R_middle_point+w/2;
+    X_big_round=R_big_round*cos(t)+Xoffset;
+    Y_big_round=R_big_round*sin(t)+Yoffset;
 
 # small round
-r1=l*cot(phi)-w/2
-#x1=linspace(-l*cot(phi),-w/2,100);
-# the x value when y=0
-x1=linspace(-sqrt(power(r1,2)-power(d,2))-x_orig,+sqrt(power(r1,2)-power(d,2))-x_orig,100)
-y1=sqrt(power(r1,2)-power((x1+l*cot(phi)),2))-d
+    R_small_round=R_middle_point-w/2;
+    X_small_round=R_small_round*cos(t)+Xoffset;
+    Y_small_round=R_small_round*sin(t)+Yoffset;
 
-plot(x1,y1);
-hold on;
-plot(x,y);
-#hold on;
-print -dpdf track.pdf
+    step=5;
 
-#value1=hscreen/(2*sin(a));
-#smallcircler=l*cot(phi)-w/2;
-#t=linspace(asin(d/smallcircler),pi-asin(d/smallcircler),100);
-#smallcirclex=smallcircler*cos(t)-l*cot(phi);
-#smallcircley=smallcircler*sin(t)-d;
-#plot(smallcirclex,smallcircley);
-#hold on;
+    phi=i*step/180*pi;
+    plot(X_big_round,Y_big_round,"r",'LineWidth',5);
+    hold on
+    plot(X_small_round,Y_small_round,"r",'LineWidth',5);
+    hold on
+    filename=strcat("track",num2str(i*step),".jpg");
+    #print -dpdf track.pdf
+    print(filename,"-djpg")
+end
 
-#t=linspace(asin(d/smallcircler)-pi/180,pi-asin(d/smallcircler)-pi/180,100);
-#screenrightcirclex=((smallcircler*cos(t)-l*cot(phi))*w)./((smallcircler*sin(t)-d)*tan(a)*2)
-#screenrightcircley=sin(a+b-atan(h./(smallcircler*sin(t)-d)))./cos(b-atan(h./(smallcircler*sin(t)-d))).*(smallcircler*cos(t)-l*cot(phi))*value1
-#plot(screenrightcirclex,screenrightcircley);
-#hold on;
-
-#bigcircler=l*cot(phi)+w/2;
-#t=linspace(asin(d/bigcircler),pi-asin(d/bigcircler),100);
-#bigcirclex=bigcircler*cos(t)-l*cot(phi);
-#bigcircley=bigcircler*sin(t)-d;
-#plot(bigcirclex,bigcircley);
-#hold on;
-
-#t=linspace(asin(d/bigcircler)-pi/180,pi-asin(d/bigcircler)-pi/180,100);
-#screenleftcirclex=((bigcircler*cos(t)-l*cot(phi))*w)./((bigcircler*sin(t)-d)*tan(a)*2)
-#screenleftcircley=sin(a+b-atan(h./(bigcircler*sin(t)-d)))./cos(b-atan(h./(bigcircler*sin(t)-d))).*(bigcircler*cos(t)-l*cot(phi))*value1
-#plot(screenleftcirclex,screenleftcircley);
-#axis([-0.05,0.05,-0.1,0.1]);
-#print -dpdf track.pdf
